221c09f65767b86bd644f3a940731e0b243c6205
Ville Misaki <ville@misaki.fi>
2020-07-04 05:43:57 +0100

    Add prototyping for DAO.

--- server/routes.js
+++ server/routes.js
@@ -1,12 +1,16 @@
 
+
 module.exports = (app) => {
+    const db = require('./db');
+
     app.get("/api/stats", (req, res) => {
         // TODO: implement
     });
 
     app.get("/api/galleries", (req, res) => {
         // TODO: implement: list of galleries
-        res.status(501).end();
+        const galleries = db.loadGalleries();
+        res.json(galleries);
     });
     app.post("/api/galleries", (req, res) => {
         // TODO: implement: create new gallery
@@ -14,7 +18,9 @@ module.exports = (app) => {
     });
     app.get("/api/galleries/:galleryId", (req, res) => {
         // TODO: implement: list of photos in gallery, by year/month/day
-        res.status(501).end();
+        const gallery = db.loadGallery(req.params.galleryId);
+        res.json(gallery);
+        // res.status(501).end();
     });
     app.put("/api/galleries/:galleryId", (req, res) => {
         // TODO: implement: update gallery meta
@@ -44,6 +50,7 @@ module.exports = (app) => {
 
     app.get("/api/photos/", (req, res) => {
         // TODO: implement: list all photos
+        const photos = db.loadPhotos();
         res.status(501).end();
     });
     app.post("/api/photos/", (req, res) => {
@@ -52,6 +59,7 @@ module.exports = (app) => {
     });
     app.get("/api/photos/:photoId", (req, res) => {
         // TODO: implement: get photo meta
+        const photo = db.loadPhoto(req.params.photoId);
         res.status(501).end();
     });
     app.put("/api/photos/:photoId", (req, res) => {
