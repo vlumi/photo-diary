b543e8899a3989706b05814a9a88851a444c3b4d
Ville Misaki <ville@misaki.fi>
2020-07-08 16:32:40 +0900

    Clean up the logger timestamp generation.

--- converter/logger.js
+++ converter/logger.js
@@ -1,28 +1,13 @@
 const timeStamp = () => {
   const toIsoString = (date) => {
-    var tzo = -date.getTimezoneOffset(),
-      dif = tzo >= 0 ? "+" : "-",
-      pad = function (num) {
-        var norm = Math.floor(Math.abs(num));
-        return (norm < 10 ? "0" : "") + norm;
-      };
-    return (
-      date.getFullYear() +
-      "-" +
-      pad(date.getMonth() + 1) +
-      "-" +
-      pad(date.getDate()) +
-      "T" +
-      pad(date.getHours()) +
-      ":" +
-      pad(date.getMinutes()) +
-      ":" +
-      pad(date.getSeconds()) +
-      dif +
-      pad(tzo / 60) +
-      ":" +
-      pad(tzo % 60)
-    );
+    const y = String(date.getFullYear()).padStart(4, "0");
+    const m = String(date.getMonth() + 1).padStart(2, "0");
+    const d = String(date.getDate()).padStart(2, "0");
+    const hh = String(date.getHours()).padStart(2, "0");
+    const mm = String(date.getMinutes()).padStart(2, "0");
+    const ss = String(date.getSeconds()).padStart(2, "0");
+    const ms = String(date.getMilliseconds()).padEnd(3, "0");
+    return `${y}-${m}-${d} ${hh}:${mm}:${ss}.${ms}`;
   };
   return toIsoString(new Date());
 };
