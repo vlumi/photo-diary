116dbdf45d8098a6293928187c85cd37cfafb450
Ville Misaki <ville@misaki.fi>
2020-07-13 12:34:33 +0100

    Remove debug message.

--- server/utils/config/index.js
+++ server/utils/config/index.js
@@ -3,7 +3,6 @@ require("dotenv").config();
 const CONST = require("../constants");
 
 const ENV = process.env.NODE_ENV || CONST.DEFAULT_ENV;
-console.log("Loading configuration in ", ENV);
 
 const values = require(`./${ENV}-config`);
 if (!values.SECRET) {
