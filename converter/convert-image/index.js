2cb05bf2476e94e354be676b64a1bd11d705ee51
Ville Misaki <ville@misaki.fi>
2020-07-22 15:02:18 +0100

    Fix converter to chmod the produced files as 0644.

--- converter/convert-image/index.js
+++ converter/convert-image/index.js
@@ -1,5 +1,6 @@
 const gm = require("gm").subClass({ imageMagick: true });
 const path = require("path");
+const fs = require("fs");
 
 const CONST = require("../utils/constants");
 const logger = require("../utils/logger");
@@ -16,10 +17,11 @@ module.exports = (fileName, root, target) => {
     gm(inputPath)
       .autoOrient()
       .resize(width, height)
-      .write(outputPath, (error) => {
+      .write(outputPath, async (error) => {
         if (error) {
           reject(error);
         } else {
+          await fs.promises.chmod(outputPath, "0644");
           resolve();
         }
       });
