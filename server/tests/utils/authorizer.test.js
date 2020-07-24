379be92ae95874c66c043b79bb77427e3bfdd96e
Ville Misaki <ville@misaki.fi>
2020-07-24 15:01:15 +0100

    Add more unit tests for authorizer.

--- server/tests/utils/authorizer.test.js
+++ server/tests/utils/authorizer.test.js
@@ -9,6 +9,11 @@ const {
 const db = require("../../db");
 jest.mock("../../db");
 
+const fail = (authorize, user, gallery) => {
+  expect.assertions(1);
+  return authorize(user, gallery).catch((e) => expect(e).toMatch("Access"));
+};
+
 describe("No ACL defined", () => {
   beforeAll(() => {
     db.loadUserAccessControl.mockImplementation((user) => {
@@ -19,48 +24,17 @@ describe("No ACL defined", () => {
     });
   });
   describe("As :guest", () => {
-    test("Authorize view", () => {
-      expect.assertions(1);
-      return authorizeView(":guest").catch((e) => expect(e).toMatch("Access"));
-    });
-    test("Authorize admin", () => {
-      expect.assertions(1);
-      return authorizeAdmin(":guest").catch((e) => expect(e).toMatch("Access"));
-    });
-    test("Authorize gallery view", () => {
-      expect.assertions(1);
-      return authorizeGalleryView(":guest", "gallery").catch((e) =>
-        expect(e).toMatch("Access")
-      );
-    });
-    test("Authorize gallery admin", () => {
-      expect.assertions(1);
-      return authorizeGalleryAdmin(":guest", "gallery").catch((e) =>
-        expect(e).toMatch("Access")
-      );
-    });
+    test("View", () => fail(authorizeView, ":guest"));
+    test("Admin", () => fail(authorizeAdmin, ":guest"));
+    test("Gallery view", () => fail(authorizeGalleryView, ":guest", "gallery"));
+    test("Gallery admin", () =>
+      fail(authorizeGalleryAdmin, ":guest", "gallery"));
   });
   describe("As user", () => {
-    test("Authorize view", () => {
-      expect.assertions(1);
-      return authorizeView("user").catch((e) => expect(e).toMatch("Access"));
-    });
-    test("Authorize admin", () => {
-      expect.assertions(1);
-      return authorizeAdmin("user").catch((e) => expect(e).toMatch("Access"));
-    });
-    test("Authorize gallery view", () => {
-      expect.assertions(1);
-      return authorizeGalleryView("user", "gallery").catch((e) =>
-        expect(e).toMatch("Access")
-      );
-    });
-    test("Authorize gallery admin", () => {
-      expect.assertions(1);
-      return authorizeGalleryAdmin("user", "gallery").catch((e) =>
-        expect(e).toMatch("Access")
-      );
-    });
+    test("View", () => fail(authorizeView, "user"));
+    test("Admin", () => fail(authorizeAdmin, "user"));
+    test("Gallery view", () => fail(authorizeGalleryView, "user", "gallery"));
+    test("Gallery admin", () => fail(authorizeGalleryAdmin, "user", "gallery"));
   });
 });
 
@@ -76,40 +50,17 @@ describe("ACL :guest,:all,VIEW", () => {
     });
   });
   describe("As :guest", () => {
-    test("Authorize view", () => {
-      return authorizeView(":guest");
-    });
-    test("Authorize admin", () => {
-      expect.assertions(1);
-      return authorizeAdmin(":guest").catch((e) => expect(e).toMatch("Access"));
-    });
-    test("Authorize gallery view", () => {
-      return authorizeGalleryView(":guest", "gallery");
-    });
-    test("Authorize gallery admin", () => {
-      expect.assertions(1);
-      return authorizeGalleryAdmin(":guest", "gallery").catch((e) =>
-        expect(e).toMatch("Access")
-      );
-    });
+    test("View", () => authorizeView(":guest"));
+    test("Admin", () => fail(authorizeAdmin, ":guest"));
+    test("Gallery view", () => authorizeGalleryView(":guest", "gallery"));
+    test("Gallery admin", () =>
+      fail(authorizeGalleryAdmin, ":guest", "gallery"));
   });
   describe("As user", () => {
-    test("Authorize view", () => {
-      return authorizeView("user");
-    });
-    test("Authorize admin", () => {
-      expect.assertions(1);
-      return authorizeAdmin("user").catch((e) => expect(e).toMatch("Access"));
-    });
-    test("Authorize gallery view", () => {
-      return authorizeGalleryView("user", "gallery");
-    });
-    test("Authorize gallery admin", () => {
-      expect.assertions(1);
-      return authorizeGalleryAdmin("user", "gallery").catch((e) =>
-        expect(e).toMatch("Access")
-      );
-    });
+    test("View", () => authorizeGalleryView("user", "gallery"));
+    test("Admin", () => fail(authorizeAdmin, "user"));
+    test("Gallery view", () => authorizeGalleryView("user", "gallery"));
+    test("Gallery admin", () => fail(authorizeGalleryAdmin, "user", "gallery"));
   });
 });
 
@@ -125,32 +76,16 @@ describe("ACL :guest,:all,ADMIN", () => {
     });
   });
   describe("As :guest", () => {
-    test("Authorize view", () => {
-      return authorizeView(":guest");
-    });
-    test("Authorize admin", () => {
-      return authorizeAdmin(":guest");
-    });
-    test("Authorize gallery view", () => {
-      return authorizeGalleryView(":guest", "gallery");
-    });
-    test("Authorize gallery admin", () => {
-      return authorizeGalleryAdmin(":guest", "gallery");
-    });
+    test("View", () => authorizeView(":guest"));
+    test("Admin", () => authorizeAdmin(":guest"));
+    test("Gallery view", () => authorizeGalleryView(":guest", "gallery"));
+    test("Gallery admin", () => authorizeGalleryAdmin(":guest", "gallery"));
   });
   describe("As user", () => {
-    test("Authorize view", () => {
-      return authorizeView("user");
-    });
-    test("Authorize admin", () => {
-      return authorizeAdmin("user");
-    });
-    test("Authorize gallery view", () => {
-      return authorizeGalleryView("user", "gallery");
-    });
-    test("Authorize gallery admin", () => {
-      return authorizeGalleryAdmin("user", "gallery");
-    });
+    test("View", () => authorizeView("user"));
+    test("Admin", () => authorizeAdmin("user"));
+    test("Gallery view", () => authorizeGalleryView("user", "gallery"));
+    test("Gallery admin", () => authorizeGalleryAdmin("user", "gallery"));
   });
 });
 
@@ -166,44 +101,17 @@ describe("ACL :user,:all,VIEW", () => {
     });
   });
   describe("As :guest", () => {
-    test("Authorize view", () => {
-      expect.assertions(1);
-      return authorizeView(":guest").catch((e) => expect(e).toMatch("Access"));
-    });
-    test("Authorize admin", () => {
-      expect.assertions(1);
-      return authorizeAdmin(":guest").catch((e) => expect(e).toMatch("Access"));
-    });
-    test("Authorize gallery view", () => {
-      expect.assertions(1);
-      return authorizeGalleryView(":guest", "gallery").catch((e) =>
-        expect(e).toMatch("Access")
-      );
-    });
-    test("Authorize gallery admin", () => {
-      expect.assertions(1);
-      return authorizeGalleryAdmin(":guest", "gallery").catch((e) =>
-        expect(e).toMatch("Access")
-      );
-    });
+    test("View", () => fail(authorizeView, ":guest"));
+    test("Admin", () => fail(authorizeAdmin, ":guest"));
+    test("Gallery view", () => fail(authorizeGalleryView, ":guest", "gallery"));
+    test("Gallery admin", () =>
+      fail(authorizeGalleryAdmin, ":guest", "gallery"));
   });
   describe("As user", () => {
-    test("Authorize view", () => {
-      return authorizeView("user");
-    });
-    test("Authorize admin", () => {
-      expect.assertions(1);
-      return authorizeAdmin("user").catch((e) => expect(e).toMatch("Access"));
-    });
-    test("Authorize gallery view", () => {
-      return authorizeGalleryView("user", "gallery");
-    });
-    test("Authorize gallery admin", () => {
-      expect.assertions(1);
-      return authorizeGalleryAdmin("user", "gallery").catch((e) =>
-        expect(e).toMatch("Access")
-      );
-    });
+    test("View", () => authorizeGalleryView("user", "gallery"));
+    test("Admin", () => fail(authorizeAdmin, "user"));
+    test("Gallery view", () => authorizeGalleryView("user", "gallery"));
+    test("Gallery admin", () => fail(authorizeGalleryAdmin, "user", "gallery"));
   });
 });
 
@@ -219,39 +127,202 @@ describe("ACL :user,:all,ADMIN", () => {
     });
   });
   describe("As :guest", () => {
-    test("Authorize view", () => {
-      expect.assertions(1);
-      return authorizeView(":guest").catch((e) => expect(e).toMatch("Access"));
-    });
-    test("Authorize admin", () => {
-      expect.assertions(1);
-      return authorizeAdmin(":guest").catch((e) => expect(e).toMatch("Access"));
-    });
-    test("Authorize gallery view", () => {
-      expect.assertions(1);
-      return authorizeGalleryView(":guest", "gallery").catch((e) =>
-        expect(e).toMatch("Access")
-      );
-    });
-    test("Authorize gallery admin", () => {
-      expect.assertions(1);
-      return authorizeGalleryAdmin(":guest", "gallery").catch((e) =>
-        expect(e).toMatch("Access")
-      );
-    });
+    test("View", () => fail(authorizeView, ":guest"));
+    test("Admin", () => fail(authorizeAdmin, ":guest"));
+    test("Gallery view", () => fail(authorizeGalleryView, ":guest", "gallery"));
+    test("Gallery admin", () =>
+      fail(authorizeGalleryAdmin, ":guest", "gallery"));
   });
   describe("As user", () => {
-    test("Authorize view", () => {
-      return authorizeView("user");
+    test("View", () => authorizeView("user"));
+    test("Admin", () => authorizeAdmin("user"));
+    test("Gallery view", () => authorizeGalleryView("user", "gallery"));
+    test("Gallery admin", () => authorizeGalleryAdmin("user", "gallery"));
+  });
+});
+
+describe("ACL :guest,:all,VIEW, user,:all,ADMIN", () => {
+  beforeAll(() => {
+    db.loadUserAccessControl.mockImplementation((user) => {
+      switch (user) {
+        case ":guest":
+          return { ":all": CONST.ACCESS_VIEW };
+        case "user":
+          return { ":all": CONST.ACCESS_ADMIN };
+        default:
+          return {};
+      }
     });
-    test("Authorize admin", () => {
-      return authorizeAdmin("user");
+  });
+  describe("As :guest", () => {
+    test("View", () => authorizeView(":guest"));
+    test("Admin", () => fail(authorizeAdmin, ":guest"));
+    test("Gallery view", () => authorizeGalleryView(":guest", "gallery"));
+    test("Gallery admin", () =>
+      fail(authorizeGalleryAdmin, ":guest", "gallery"));
+  });
+  describe("As user", () => {
+    test("View", () => authorizeView("user"));
+    test("Admin", () => authorizeAdmin("user"));
+    test("Gallery view", () => authorizeGalleryView("user", "gallery"));
+    test("Gallery admin", () => authorizeGalleryAdmin("user", "gallery"));
+  });
+});
+
+describe("ACL :guest,:all,ADMIN, user1,:all,VIEW", () => {
+  beforeAll(() => {
+    db.loadUserAccessControl.mockImplementation((user) => {
+      switch (user) {
+        case ":guest":
+          return { ":all": CONST.ACCESS_ADMIN };
+        case "user1":
+          return { ":all": CONST.ACCESS_VIEW };
+        default:
+          return {};
+      }
     });
-    test("Authorize gallery view", () => {
-      return authorizeGalleryView("user", "gallery");
+  });
+  describe("As :guest", () => {
+    test("View", () => authorizeView(":guest"));
+    test("Admin", () => authorizeAdmin(":guest"));
+    test("Gallery view", () => authorizeGalleryView(":guest", "gallery"));
+    test("Gallery admin", () => authorizeGalleryAdmin(":guest", "gallery"));
+  });
+  describe("As user1", () => {
+    test("View", () => authorizeGalleryView("user1", "gallery"));
+    test("Admin", () => fail(authorizeAdmin, "user1"));
+    test("Gallery view", () => authorizeGalleryView("user1", "gallery"));
+    test("Gallery admin", () =>
+      fail(authorizeGalleryAdmin, "user1", "gallery"));
+  });
+  describe("As user2", () => {
+    test("View", () => authorizeView("user2"));
+    test("Admin", () => authorizeAdmin("user2"));
+    test("Gallery view", () => authorizeGalleryView("user2", "gallery"));
+    test("Gallery admin", () => authorizeGalleryAdmin("user2", "gallery"));
+  });
+});
+
+describe("ACL :guest,:all,ADMIN, user1,:all,NONE", () => {
+  beforeAll(() => {
+    db.loadUserAccessControl.mockImplementation((user) => {
+      switch (user) {
+        case ":guest":
+          return { ":all": CONST.ACCESS_ADMIN };
+        case "user1":
+          return { ":all": CONST.ACCESS_NONE };
+        default:
+          return {};
+      }
     });
-    test("Authorize gallery admin", () => {
-      return authorizeGalleryAdmin("user", "gallery");
+  });
+  describe("As :guest", () => {
+    test("View", () => authorizeView(":guest"));
+    test("Admin", () => authorizeAdmin(":guest"));
+    test("Gallery view", () => authorizeGalleryView(":guest", "gallery"));
+    test("Gallery admin", () => authorizeGalleryAdmin(":guest", "gallery"));
+  });
+  describe("As user1", () => {
+    test("View", () => fail(authorizeView, "user1"));
+    test("Admin", () => fail(authorizeAdmin, "user1"));
+    test("Gallery view", () => fail(authorizeGalleryView, "user1", "gallery"));
+    test("Gallery admin", () =>
+      fail(authorizeGalleryAdmin, "user1", "gallery"));
+  });
+  describe("As user2", () => {
+    test("View", () => authorizeView("user2"));
+    test("Admin", () => authorizeAdmin("user2"));
+    test("Gallery view", () => authorizeGalleryView("user2", "gallery"));
+    test("Gallery admin", () => authorizeGalleryAdmin("user2", "gallery"));
+  });
+});
+
+describe("ACL :guest,:all,VIEW, user1,gallery1,ADMIN", () => {
+  beforeAll(() => {
+    db.loadUserAccessControl.mockImplementation((user) => {
+      switch (user) {
+        case ":guest":
+          return { ":all": CONST.ACCESS_VIEW };
+        case "user1":
+          return { gallery1: CONST.ACCESS_ADMIN };
+        default:
+          return {};
+      }
     });
+  });
+  describe("As :guest", () => {
+    test("View", () => authorizeView(":guest"));
+    test("Admin", () => fail(authorizeAdmin, ":guest"));
+    test("Gallery1 view", () => authorizeGalleryView(":guest", "gallery1"));
+    test("Gallery1 admin", () =>
+      fail(authorizeGalleryAdmin, ":guest", "gallery1"));
+    test("Gallery2 view", () => authorizeGalleryView(":guest", "gallery2"));
+    test("Gallery2 admin", () =>
+      fail(authorizeGalleryAdmin, ":guest", "gallery2"));
+  });
+  describe("As user1", () => {
+    test("View", () => authorizeView("user1"));
+    test("Admin", () => fail(authorizeAdmin, "user1"));
+    test("Gallery1 view", () => authorizeGalleryView("user1", "gallery1"));
+    test("Gallery1 admin", () => authorizeGalleryAdmin("user1", "gallery1"));
+    test("Gallery2 view", () => authorizeGalleryView("user1", "gallery2"));
+    test("Gallery2 admin", () =>
+      fail(authorizeGalleryAdmin, "user1", "gallery2"));
+  });
+  describe("As user2", () => {
+    test("View", () => authorizeView("user2"));
+    test("Admin", () => fail(authorizeAdmin, "user2"));
+    test("Gallery1 view", () => authorizeGalleryView("user2", "gallery1"));
+    test("Gallery1 admin", () =>
+      fail(authorizeGalleryAdmin, "user2", "gallery1"));
+    test("Gallery2 view", () => authorizeGalleryView("user2", "gallery2"));
+    test("Gallery2 admin", () =>
+      fail(authorizeGalleryAdmin, "user2", "gallery2"));
+  });
+});
+
+describe("ACL :guest,:all,NONE,gallery2,VIEW, user1,gallery1,ADMIN", () => {
+  beforeAll(() => {
+    db.loadUserAccessControl.mockImplementation((user) => {
+      switch (user) {
+        case ":guest":
+          return { ":all": CONST.ACCESS_NONE, gallery2: CONST.ACCESS_VIEW };
+        case "user1":
+          return { gallery1: CONST.ACCESS_ADMIN };
+        default:
+          return {};
+      }
+    });
+  });
+  describe("As :guest", () => {
+    test("View", () => fail(authorizeView, ":guest"));
+    test("Admin", () => fail(authorizeAdmin, ":guest"));
+    test("Gallery1 view", () =>
+      fail(authorizeGalleryView, ":guest", "gallery1"));
+    test("Gallery1 admin", () =>
+      fail(authorizeGalleryAdmin, ":guest", "gallery1"));
+    test("Gallery2 view", () => authorizeGalleryView(":guest", "gallery2"));
+    test("Gallery2 admin", () =>
+      fail(authorizeGalleryAdmin, ":guest", "gallery2"));
+  });
+  describe("As user1", () => {
+    test("View", () => fail(authorizeView, "user1"));
+    test("Admin", () => fail(authorizeAdmin, "user1"));
+    test("Gallery1 view", () => authorizeGalleryView("user1", "gallery1"));
+    test("Gallery1 admin", () => authorizeGalleryAdmin("user1", "gallery1"));
+    test("Gallery2 view", () => authorizeGalleryView("user1", "gallery2"));
+    test("Gallery2 admin", () =>
+      fail(authorizeGalleryAdmin, "user1", "gallery2"));
+  });
+  describe("As user2", () => {
+    test("View", () => fail(authorizeView, "user2"));
+    test("Admin", () => fail(authorizeAdmin, "user2"));
+    test("Gallery1 view", () =>
+      fail(authorizeGalleryView, "user2", "gallery1"));
+    test("Gallery1 admin", () =>
+      fail(authorizeGalleryAdmin, "user2", "gallery1"));
+    test("Gallery2 view", () => authorizeGalleryView("user2", "gallery2"));
+    test("Gallery2 admin", () =>
+      fail(authorizeGalleryAdmin, "user2", "gallery2"));
   });
 });
